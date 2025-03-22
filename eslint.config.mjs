d61f1b6bd6baa2d3875f770974e984a7aa72a64f
Basile Paoli <basile.paoli@gmail.com>
2025-03-22 16:56:20 +0100

    ci: edit eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,16 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import unusedImports from 'eslint-plugin-unused-imports';
 
-export default tseslint.config(
-	eslint.configs.recommended,
-	tseslint.configs.recommended,
-	{
-		plugins: {
-			'unused-imports': unusedImports
-		},
-		rules: {
-			'unused-imports/no-unused-imports': 'error',
-		},
-	}
-);
+export default tseslint.config({
+	files: ['**/*.ts'],
+	extends: [
+		eslint.configs.recommended,
+		tseslint.configs.recommended,
+	],
+	plugins: {
+		'unused-imports': unusedImports
+	},
+	rules: {
+		'unused-imports/no-unused-imports': 'error',
+	},
+})
