45e0979b306ebfeb4e2a9548a568971fcbcaabbc
Basile Paoli <basile.paoli@gmail.com>
2025-03-26 18:49:44 +0100

    add doc to users list

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,17 @@
 import {Authorized, Get, JsonController, QueryParam} from "routing-controllers";
 import {getUsers} from "../services/users/listUsers";
-import {zListUsersParams} from "../validators/users";
-import {RequestParams} from "../open-api/decorators";
+import {zListUsersParams, zPublicUser} from "../validators/users";
+import {RequestParams, ResponseBody} from "../open-api/decorators";
+import type {PublicUser} from "../db/schema";
+import {z} from "zod";
 
 @JsonController('/users')
 @Authorized('admin')
-@RequestParams(zListUsersParams)
 export class UserController {
 	@Get('/')
-	async getUsers(@QueryParam('role') role: unknown) {
+	@RequestParams(zListUsersParams)
+	@ResponseBody(200, z.array(zPublicUser))
+	async getUsers(@QueryParam('role') role: unknown): Promise<PublicUser[]> {
 		const listUsersParams = zListUsersParams.parse({role});
 		return getUsers(listUsersParams);
 	}
--- src/validators/users.ts
+++ src/validators/users.ts
@@ -1,4 +1,6 @@
 import {z} from "zod";
+import type {PublicUser} from "../db/schema";
+import type {AssertTrue, IsExact} from "conditional-type-checks";
 
 const zUserRole = z.enum(['admin', 'customer'])
 export const zListUsersParams = z.object({
@@ -6,3 +8,12 @@ export const zListUsersParams = z.object({
 })
 
 export type ListUsersParams = z.infer<typeof zListUsersParams>
+
+export const zPublicUser = z.object({
+	id: z.number().int(),
+	email: z.string().email(),
+	role: zUserRole
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type TestPublicUser = AssertTrue<IsExact<z.infer<typeof zPublicUser>, PublicUser>>
