00735b0ae4e78b243e77b0462665ae15e24fc58b
Spatulox <M78stormtrooper@laposte.net>
2025-04-03 16:22:07 +0200

    update the create ticket validator

--- src/validators/tickets.ts
+++ src/validators/tickets.ts
@@ -9,16 +9,18 @@ export const zTicket = z.object({
     type: z.enum(["super", "normal"])
 })
 
-export const zTicketCreate = z.object({
-    id: z.number().int().positive().openapi({example: 1}),
-    userId: z.number().int().positive(),
-    type: z.enum(["super", "normal"])
+export const zCreateTicketParams = z.object({
+    type: z.enum(["super", "normal"]),
+    used: z.number().int().positive().nullable().openapi({example: 9}),
+    max_usage: z.number().int().positive().openapi({example: 10}),
 })
 
-export const zTicketUpdate = z.object({
+export const zUpdateTicketParams = z.object({
     id: z.number().int().positive().openapi({example: 1})
 })
 
 export type Ticket = z.infer<typeof zTicket>;
 export const zMyTicket = zTicket.omit({userId: true})
 export type MyTicket = z.infer<typeof zMyTicket>;
+export type CreateTicketParam = z.infer<typeof zCreateTicketParams>
+export type UpdateTicketParam = z.infer<typeof zUpdateTicketParams>
