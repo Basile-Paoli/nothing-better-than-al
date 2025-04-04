36c80b2a90c9d953fb3448aebb13bbd9abcd8e6d
Spatulox <M78stormtrooper@laposte.net>
2025-04-04 09:57:11 +0200

    add balance table

---
+++ src/db/schema/balances.ts
@@ -0,0 +1,17 @@
+import { pgTable, serial, integer, date } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { userTable } from "./user";
+
+export const balanceTable = pgTable('balance', {
+    id: serial().primaryKey(),
+    user_id: integer().notNull(),
+    deposit_amount: integer().notNull(),
+    deposit_date: date()
+});
+
+export const balanceRelations = relations(balanceTable, ({one}) => ({
+    user: one(userTable, {
+        fields: [balanceTable.user_id],
+        references: [userTable.id],
+    })
+}))
