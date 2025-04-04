1da501ee23e4c58701960b60bf1b994e44a83ce8
Spatulox <M78stormtrooper@laposte.net>
2025-04-04 09:45:53 +0200

    update db user part to have a balance linked to the user

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,4 +1,4 @@
-import {pgEnum, pgTable, serial, varchar} from "drizzle-orm/pg-core";
+import {pgEnum, pgTable, serial, varchar, integer} from "drizzle-orm/pg-core";
 import {getTableColumns, type InferSelectModel, relations} from "drizzle-orm";
 import {tokenTable} from "./token";
 
@@ -8,7 +8,8 @@ export const userTable = pgTable('user', {
 	id: serial().primaryKey(),
 	email: varchar({length: 120}).notNull().unique(),
 	password: varchar({length: 120}).notNull(),
-	role: roleEnum().notNull()
+	role: roleEnum().notNull(),
+	balance : integer().default(0)
 })
 
 export const userRelations = relations(userTable, ({many}) => ({
@@ -19,7 +20,12 @@ export const userRelations = relations(userTable, ({many}) => ({
 type UserTable = typeof userTable;
 
 const {password, ...rest} = getTableColumns(userTable)
-export const userPublicColumns = rest satisfies Omit<UserTable['_']['columns'], 'password'>
+//export const userPublicColumns = rest satisfies Omit<UserTable['_']['columns'], 'password'>
+export const userPublicColumns = rest satisfies Omit<
+  UserTable["_"]["columns"],
+  "password" | "balance"
+>;
 
 export type User = InferSelectModel<UserTable>
-export type PublicUser = Omit<User, 'password'>;
+//export type PublicUser = Omit<User, 'password'>;
+export type PublicUser = Omit<User, "password" | "balance">;
