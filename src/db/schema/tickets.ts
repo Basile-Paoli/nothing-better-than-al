6c72c3dd7498efaa0cacc57b729b4afe08920d70
Spatulox <M78stormtrooper@laposte.net>
2025-04-04 11:23:01 +0200

    rename typeEnum to ticketTypeEnum

--- src/db/schema/tickets.ts
+++ src/db/schema/tickets.ts
@@ -2,14 +2,14 @@ import { pgTable, serial, integer, date, pgEnum } from "drizzle-orm/pg-core";
 import { userTable } from "./user";
 import { relations } from "drizzle-orm";
 
-export const typeEnum = pgEnum('type', ['super', 'normal']);
+export const ticketTypeEnum = pgEnum('type', ['super', 'normal']);
 
 export const ticketTable = pgTable('tickets', {
     id: serial().primaryKey(),
     userId: integer().notNull().references(() => userTable.id),
     max_usage: integer().notNull(),
     used: integer().notNull().default(0),
-    type: typeEnum().notNull().default("normal"),
+    type: ticketTypeEnum().notNull().default("normal"),
     buy_date: date().notNull().defaultNow()
 });
 
