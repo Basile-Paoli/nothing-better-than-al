070c47fc45a9773e718ee7768dbe37fcdd0657d0
Spatulox <M78stormtrooper@laposte.net>
2025-04-07 13:59:30 +0200

    Removes unused schema property
    Removes the `screen_id` property from the `zBookSessionsParams` schema as it is no longer needed.

--- src/validators/sessions.ts
+++ src/validators/sessions.ts
@@ -5,7 +5,6 @@ import { zMovie } from "./movies"
 export const zBookSessionsParams = z.object({
 	ticket_id_used: z.number().int().positive().openapi({example: 1}),
 	nb_place_to_book: z.number().int().positive().openapi({example: 1}),
-	screen_id:  z.number().int().positive().openapi({example: 1}),
 })
 
 export const zSessions = z.object({
